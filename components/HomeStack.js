1c0daac27c7287fc5c992737a47f67cb6e9007f8
moose <moosecodes@gmail.com>
2023-06-05 12:02:18 -0700

    update button label

--- components/HomeStack.js
+++ components/HomeStack.js
@@ -17,7 +17,7 @@ function HomeStack() {
         <Button size="lg" href="https://www.linkedin.com/in/mustafaishaq/" target="_blank" variant="secondary">LinkedIn Profile</Button>
         <Button size="lg" href="https://www.github.com/moosecodes/" target="_blank" variant="secondary">Github Profile</Button>
         <Button size="lg" href="https://drive.google.com/file/d/1BEk6qDZ5Ul7Tj_MEEE4bJbsLg5e0rzMH/view" target="_blank" variant="outline-secondary">Resume</Button>
-        <Button size="lg" href="/about" variant="outline-secondary">About this site</Button>
+        <Button size="lg" href="/about" variant="outline-secondary">About</Button>
         <Button size="lg" href="/blog" variant="outline-secondary" disabled>Blog</Button>
       </Stack>
     </Container>
